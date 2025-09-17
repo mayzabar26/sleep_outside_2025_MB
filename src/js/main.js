152bac8a278b8a5667250855f6feff4551d1b1e3
Mayza Barbosa <mayzabar26@gmail.com>
2025-09-17 11:55:25 -0300

    W03 Team Activity: Dynamic Header and Footer

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,9 @@
 import ProductData from './ProductData.mjs';
 import ProductList from './ProductList.mjs';
-import { getParam } from './utils.mjs';
+import { getParam, loadHeaderFooter } from './utils.mjs';
+
+loadHeaderFooter();
 
 const dataSource = new ProductData('tents');
-
 const productList = new ProductList('tents', dataSource, document.querySelector('.product-list'));
 productList.init();
